93f2cbdab0767c12e9957f5d6ed174328d7cb695
Ankit Chaudhary <ankit.chaudhary@tietoevry.com>
2022-02-18 20:00:16 +0530

    Simple Rest service with Test Cases,Swagger,Response Status

--- DemoCXF/src/main/java/te/cxf/DemoCXF/DemoCxfApplication.java
+++ DemoCXF/src/main/java/te/cxf/DemoCXF/DemoCxfApplication.java
@@ -2,12 +2,29 @@ package te.cxf.DemoCXF;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class DemoCxfApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoCxfApplication.class, args);
+	}
+
+	@Bean
+	public Docket studentAPI() {
+		return new Docket(DocumentationType.OAS_30)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("te.cxf.DemoCXF"))
+				.paths(PathSelectors.any())
+				.build();
 	}
 
 }
--- DemoCXF/src/main/java/te/cxf/DemoCXF/endpoint/EmployeeRestController.java
+++ DemoCXF/src/main/java/te/cxf/DemoCXF/endpoint/EmployeeRestController.java
@@ -1,7 +1,13 @@
 package te.cxf.DemoCXF.endpoint;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import te.cxf.DemoCXF.domain.Employee;
 import te.cxf.DemoCXF.repo.EmployeeRepo;
@@ -9,41 +15,76 @@ import te.cxf.DemoCXF.service.EmployeeService;
 
 import java.util.List;
 
+@Api(value = "EmployeeServices")
 @RestController
 public class EmployeeRestController {
 
     @Autowired
-    @Qualifier("empSOAPService")
+    @Qualifier("empService")
     private EmployeeService employeeService;
 
-    @GetMapping("/test")
-    public String test(){
-        return employeeService.test();
-    }
 
+    @ApiOperation(value = "Get Employee By Id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found!!!") })
     @GetMapping("/{id}")
-    public Employee getEmployeeRequest(@PathVariable int id){
-        return employeeService.getEmployee(id);
+    public ResponseEntity<Employee> getEmployeeRequest(@PathVariable int id){
+        Employee employee = employeeService.getEmployee(id);
+        return ResponseEntity.status(HttpStatus.OK).body(employee);
     }
 
+    @ApiOperation(value = "Create Employee")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Suceess|OK"),
+            @ApiResponse(code = 201, message = "Created"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found!!!") })
     @PostMapping
-    public Employee createEmployeeRequest(@RequestBody Employee employee){
-        return employeeService.createEmployee(employee);
+    public ResponseEntity<Employee> createEmployeeRequest(@RequestBody Employee employee){
+        Employee emp = employeeService.createEmployee(employee);
+        return ResponseEntity.status(HttpStatus.CREATED).body(emp);
     }
 
+    @ApiOperation(value = "Update Employee")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Suceess|OK"),
+            @ApiResponse(code = 204, message = "No Content"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found!!!") })
     @PatchMapping
-    public Employee updateEmployeeRequest(@RequestBody Employee employee){
-        return employeeService.updateEmployee(employee);
+    public ResponseEntity updateEmployeeRequest(@RequestBody Employee employee){
+        employeeService.updateEmployee(employee);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
     }
 
+    @ApiOperation(value = "Delete Employee")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Suceess|OK"),
+            @ApiResponse(code = 204, message = "No Content"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found!!!") })
     @DeleteMapping("/{id}")
-    public void deleteEmployeeRequest(@PathVariable int id)
+    public ResponseEntity deleteEmployeeRequest(@PathVariable int id)
     {
          employeeService.deleteEmployee(id);
+         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
     }
 
+    @ApiOperation(value = "List Employee")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Suceess|OK"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found!!!") })
     @GetMapping
-    public List<Employee> listAllEmployeeRequest(){
-        return employeeService.listEmployee();
+    public ResponseEntity<List<Employee>> listAllEmployeeRequest(){
+        List<Employee> employees = employeeService.listEmployee();
+        return ResponseEntity.status(HttpStatus.OK).body(employees);
     }
 }
